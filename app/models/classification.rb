c291f68fbdc0159c2ba1662aca881c69a36a195f
prahal <vprahaladhan@gmail.com>
2021-06-25 14:22:54 +1000

    Done.

--- app/models/classification.rb
+++ app/models/classification.rb
@@ -4,10 +4,13 @@ class Classification < ActiveRecord::Base
 
   def self.my_all
     # all
+    Classification
   end
 
   def self.longest
     # Boat.longest.classifications
+    # Classification.where(:id => BoatClassification.where(boat_id: Boat.all.order(length: :desc).limit(1).select(:id)).select(:classification_id)).select(:name)
+    Boat.longest.classifications.select(:name)
   end
 
 end
